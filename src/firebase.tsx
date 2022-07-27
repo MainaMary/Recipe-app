0d08bd33391a9a70ce00c546f0a9441f2983fa0e
Mary <marieshix98@gmail.com>
2022-07-27 22:44:32 +0300

    Add enviroment variables to enforce firebase keys

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,24 +1,25 @@
 // Import the functions you need from the SDKs you need
-import { getUA } from "@firebase/util";
+import firebase from "firebase/app";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBuzFR1gZCt4SeVqprsCDcrNphLoqjj8BQ",
-  authDomain: "fir-react-authentication-6bb82.firebaseapp.com",
-  projectId: "fir-react-authentication-6bb82",
-  storageBucket: "fir-react-authentication-6bb82.appspot.com",
-  messagingSenderId: "660448906967",
-  appId: "1:660448906967:web:f7306261c85c4445caaa92",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 
 //initialize firebase authentication
-export const auth = getAuth(app);
+export const auth = app.auth();
 export default app;
