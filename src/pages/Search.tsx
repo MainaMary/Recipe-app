f25204ef03fe027d2e445a23887d33e01295b60d
Mary <marieshix98@gmail.com>
2022-09-13 16:31:28 +0300

    fetch random recipes

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
-import { useEffect } from "react";
+import styled from "styled-components";
 const Search = () => {
   const fetchRecipes = async () => {
     const res = await axios.get(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}`
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`
     );
 
     return res;
@@ -15,13 +15,51 @@ const Search = () => {
     "getRecipes",
     fetchRecipes
   );
-  console.log(isSuccess && data, "data");
+  console.log(isSuccess && data?.data?.recipes, "data");
   return (
-    <div>
-      {isLoading && <p>Loading...</p>}
-      <p>Search</p>
-    </div>
+    <>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <Wrapper>
+          {isSuccess &&
+            data?.data?.recipes.map((recipe: any, index: number) => (
+              <Box key={index}>
+                <h2>{recipe.title}</h2>
+                {isLoading ? (
+                  <p>Loader..</p>
+                ) : (
+                  <img
+                    src={recipe.image}
+                    alt={recipe.title}
+                    style={{ width: "100%", height: "auto" }}
+                  />
+                )}
+              </Box>
+            ))}
+        </Wrapper>
+      )}
+    </>
   );
 };
 
 export default Search;
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  background-color: blue;
+  gap: 10px;
+  width: 100%;
+`;
+const Box = styled.div`
+  min-height: 1rem;
+  width: 400px;
+  margin: auto;
+  border-radius: 10px;
+  overflow: hidden;
+
+  img {
+    border-radius: 10px;
+  }
+`;
