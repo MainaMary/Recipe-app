bf12c4c31e7d86d45b27cd8adcc916e8efa4d53f
Mary <marieshix98@gmail.com>
2022-10-25 20:57:17 +0300

    Add modal component

---
+++ src/pages/diet/Diet.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+import CustomLoader from "../../components/CustomLoader";
+import styled from "styled-components";
+
+interface Props {
+  isLoading?: boolean;
+  diet?: any;
+  nutrients?: any[];
+}
+const Diet = (props: Props) => {
+  const { isLoading, diet, nutrients } = props;
+  return (
+    <>
+      <Navbar />
+      {
+        <Grid>
+          {isLoading ? (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "30px",
+              }}
+            >
+              <CustomLoader />
+            </div>
+          ) : (
+            <p>Diet plan</p>
+          )}
+        </Grid>
+      }
+    </>
+  );
+};
+
+export default Diet;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+const Box = styled.div`
+  padding: 10px 20px;
+  height: 300px;
+  width: 400px;A
+  margin: 12px auto;
+  border-radius: 10px;
+  overflow: hidden;
+
+  img {
+    border-radius: 10px;
+  }
+`;
