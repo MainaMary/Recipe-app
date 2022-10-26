98f48cfb24b275561ef929505ff525052c01bc6d
Mary <marieshix98@gmail.com>
2022-10-26 23:29:11 +0300

    Make styled components reusable

--- src/styles/styled.js
+++ src/styles/styled.js
@@ -4,3 +4,85 @@ display: flex,
 justify-content: center,
 marginTop: 30px,
 `;
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  height: calc(100vh - 10vh);
+  align-items: center;
+`;
+export const Main = styled.div`
+  display: flex;
+  width: 70%;
+  max-width: 1000px;
+  border-radius: 5px;
+  box-shadow: 0 0 3px;
+  @media (max-width: 800px) {
+    width: "70%";
+    max-width: 600px;
+  }
+`;
+export const Image = styled.div`
+  width: 100%;
+  text-align: center;
+  height: 300px;
+
+  img {
+    margin: auto;
+    text-align: center;
+    height: 100%;
+  }
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+export const LogIn = styled.div`
+  width: 100%;
+`;
+export const FormWrapper = styled.form`
+  padding: 12px 18px;
+  border-left: 1px solid #000;
+  @media (max-width: 800px) {
+    width: "70%";
+    padding: "12px 10px";
+  }
+`;
+export const Box = styled.div`
+  width: 100%;
+  margin: 12px 0;
+`;
+export const PasswordWrap = styled.div`
+  width: 100%;
+  @media (max-width: 800px) {
+    width: 100%;
+  }
+`;
+export const Terms = styled.div`
+  display: flex;
+  margin: 12px 0;
+  width: 100%;
+  justify-content: space-between;
+  p: {
+    margin-top: 5px;
+  }
+`;
+export const Password = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  color: #fff;
+  font-weight: bold;
+  background-color: var(--globalColor);
+  border: none;
+  outline: none;
+  font-size: 16px;
+  border-radius: 5px;
+  cursor: pointer;
+`;
