fa5438ae120715b400facf7bc24864a860ba0aae
Mary <marieshix98@gmail.com>
2022-10-28 18:59:08 +0300

    Add update user profile functionality

--- src/pages/updateProfile/UpdateProfile.tsx
+++ src/pages/updateProfile/UpdateProfile.tsx
@@ -1,73 +1,135 @@
-import React, { useRef, useState } from "react";
+import React, { useState, useContext } from "react";
 import {
   FormWrapper,
   Box,
-  Password,
-  PasswordWrap,
+  NavigateForgot,
   Button,
+  ForgotPswdWrap,
+  Password,
+  PasswordsWrapper,
 } from "../../styles/styled";
-
+import Navbar from "../../components/Navbar";
 import CustomLabel from "../../components/CustomLabel";
 import CustomInput from "../../components/CustomInput";
 import { useNavigate } from "react-router-dom";
-const UpdateProfile = () => {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [emailErr, setEmailErr] = useState("");
-  const [passwordErr, setPasswordErr] = useState("");
+import { UserContext } from "../../context/UserContext";
+import { auth } from "../../firebase";
+import { updateProfile } from "firebase/auth";
 
-  const emailRef = useRef("");
-  const passwordRef = useRef("");
-  const handleEmailChange = () => {
-    setEmail(emailRef.current);
+const UpdateProfile = () => {
+  const { currentUser } = useContext(UserContext);
+  const [email, setEmail] = useState<any>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPswd, setConfirmPswd] = useState<string>("");
+  const [emailErr, setEmailErr] = useState("");
+
+  const navigate = useNavigate();
+
+  const [error, setError] = useState("");
+  const [confirmPswdErr, setConfirmPswdErr] = useState<string>("");
+
+  const handleEmailChange = (e: any) => {
+    setEmail(e.target.value);
     setEmailErr("");
   };
-  const handlePasswordChange = () => {
-    setPassword(passwordRef.current);
-  };
-  const handleSubmit = (e: React.FormEvent) => {
+
+  //chaining promises all together
+  // const promises = [];
+  // if (currentUser?.email !== email) {
+  //   promises.push(updateEmail(auth, email));
+  // }
+  // if (password) {
+  //   promises.push(updatePassword(auth, password));
+  // }
+  // Promise.all(promises)
+  //   .then((res) => {})
+  //   .catch((err: any) => {
+  //     setError("Failed to update accounts");
+  //   })
+  //   .finally();
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
     if (!email) {
       setEmailErr("Email is required");
     }
-
-    e.preventDefault();
-    console.log(email, password);
+    if (password !== confirmPswd) {
+      setConfirmPswdErr("Passwords do not match");
+    }
+    const userData = {
+      email: email,
+      password: password,
+    };
+    try {
+      // const res = await updateProfile(userData);
+      // console.log(res, "update profile");
+    } catch (err: any) {
+      console.log(err.message);
+    }
   };
   return (
-    <FormWrapper onSubmit={handleSubmit}>
-      <div>'Error'</div>
-      <div>
-        <h2 style={{ color: "var(--globalColor)" }}>Log in</h2>
-      </div>
+    <>
+      <Navbar show={true} checkBtns={false} />
+      <ForgotPswdWrap>
+        <FormWrapper style={{ width: "100%" }}>
+          <div>{error}</div>
 
-      <Box>
-        <CustomLabel>New email</CustomLabel>
-        <CustomInput
-          ref={emailRef}
-          value={email}
-          name="email"
-          onChange={handleEmailChange}
-        />
-        <div style={{ fontSize: "12px", color: "red", margin: "5px 0" }}>
-          {emailErr ? emailErr : ""}
-        </div>
-      </Box>
-      <Password>
-        <PasswordWrap>
-          <CustomLabel>Password</CustomLabel>
-          <CustomInput
-            value={password}
-            name="password"
-            onChange={handlePasswordChange}
-          />
-          <div style={{ fontSize: "12px", color: "red", margin: "5px 0" }}>
-            {passwordErr ? passwordErr : ""}
+          <div>
+            <h2 style={{ color: "var(--globalColor)" }}>Update profile</h2>
           </div>
-        </PasswordWrap>
-      </Password>
-
-      <Box></Box>
-    </FormWrapper>
+          <Box>
+            <CustomLabel>Email</CustomLabel>
+            <CustomInput
+              value={email}
+              onChange={handleEmailChange}
+              name="username"
+            />
+            <div style={{ fontSize: "12px", color: "red", margin: "5px 0" }}>
+              {emailErr ? emailErr : ""}
+            </div>
+          </Box>
+          <Password>
+            <PasswordsWrapper>
+              <CustomLabel>Password</CustomLabel>
+              <CustomInput
+                value={password}
+                name="password"
+                onChange={(e: any) => setPassword(e.target.value)}
+              />
+            </PasswordsWrapper>
+            <PasswordsWrapper>
+              <CustomLabel>Confirm Password</CustomLabel>
+              <CustomInput
+                value={confirmPswd}
+                name="confirmPswd"
+                onChange={(e: any) => setConfirmPswd(e.target.value)}
+              />
+              <div style={{ fontSize: "12px", color: "red", margin: "5px 0" }}>
+                {confirmPswdErr ? confirmPswdErr : ""}
+              </div>
+            </PasswordsWrapper>
+          </Password>
+          <Box>
+            <Button onClick={handleSubmit}>Update profile</Button>
+          </Box>
+          <NavigateForgot>
+            <p onClick={() => navigate("/recipe")}>
+              <span
+                style={{
+                  color: "var(--globalColor)",
+                  textDecoration: "underline",
+                  marginLeft: "5px",
+                  cursor: "pointer",
+                  fontWeight: "bolder",
+                }}
+              >
+                Cancel
+              </span>
+            </p>
+          </NavigateForgot>
+        </FormWrapper>
+      </ForgotPswdWrap>
+    </>
   );
 };
 
